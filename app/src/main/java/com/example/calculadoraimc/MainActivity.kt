a7cdf0d5133bb33f66fbfad197311448f4dd1d50
Liliana <liliestafania2006@gmail.com>
2026-06-02 14:37:43 -0500

    Mi primer commit: Proyecto inicial

---
+++ app/src/main/java/com/example/calculadoraimc/MainActivity.kt
@@ -0,0 +1,15 @@
+package com.example.calculadoraimc
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import com.example.calculadoraimc.navigation.AppNavigation
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setContent {
+            Text("Calculadora IMC")
+        }
+    }
+}
---
+++ app/src/main/java/com/example/calculadoraimc/navigation/AppNavigation.kt
@@ -0,0 +1,11 @@
+package com.example.calculadoraimc.navigation
+
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavType
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import androidx.navigation.navArgument
+import com.example.calculadoraimc.screens.PantallaIngreso
+import com.example.calculadoraimc.screens.PantallaResultado
+
