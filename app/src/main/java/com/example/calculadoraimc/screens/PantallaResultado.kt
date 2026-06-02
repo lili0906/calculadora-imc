9781c44f83009832b82f5e53cbb8b115b575a17b
Liliana <liliestafania2006@gmail.com>
2026-06-02 14:56:04 -0500

    Se creo la clasificación y el boton del volver

--- app/src/main/java/com/example/calculadoraimc/screens/PantallaResultado.kt
+++ app/src/main/java/com/example/calculadoraimc/screens/PantallaResultado.kt
@@ -10,3 +10,40 @@ import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 
+@Composable
+fun PantallaResultado(
+    nombre: String,
+    imc: Float
+) {
+    when {
+
+        imc < 18.5 -> {
+            categoria = "Bajo peso"
+            color = Color.Red
+        }
+
+        imc < 25 -> {
+            categoria = "Peso normal"
+            color = Color.Green
+        }
+
+        imc < 30 -> {
+            categoria = "Sobrepeso"
+            color = Color(0xFFFF9800)
+        }
+
+        else -> {
+            categoria = "Obesidad"
+            color = Color.Red
+        }
+    }
+
+    Text(
+        text = "Hola $nombre, tu resultado es: $imc"
+    )
+    Button(
+        onClick = onVolver
+    ) {
+        Text("Volver")
+    }
+}
